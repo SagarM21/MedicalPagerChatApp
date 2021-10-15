a694667340f7fdbbf5663a6ae1f5d92424171ff7
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2021-10-15 12:56:17 +0530

    SideBar Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import {StreamChat} from 'stream-chat'
-import { Chat} from 'stream-chat-react'
-import cookies from 'universal-cookie'
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
 
-import {ChannelContainer, ChannelListContainer} from './components'
-import './App.css'
+import { ChannelContainer, ChannelListContainer } from "./components";
+import "./App.css";
 
-const apiKey= 'wegcad8shnam'
-const client = StreamChat.getInstance(apiKey)
+const apiKey = "wegcad8shnam";
+const client = StreamChat.getInstance(apiKey);
 const App = () => {
-  return (
-    <div className="app__wrapper"> 
-      <Chat client={client} theme="team light">
-        <ChannelListContainer />
+	return (
+		<div className='app__wrapper'>
+			<Chat client={client} theme='team light'>
+				<ChannelListContainer />
 
-        <ChannelContainer/>
-      </Chat>
-    </div>
-  )
-}
+				<ChannelContainer />
+			</Chat>
+		</div>
+	);
+};
 
-export default App
+export default App;
